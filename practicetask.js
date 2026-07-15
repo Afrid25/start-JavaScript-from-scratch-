861aa9c47523791bbab93d24843fb1aeaaa6fff7
Afrid25 <25afridfoisal00@gmail.com>
2026-07-15 22:06:52 +0600

    practice task

---
+++ practicetask.js
@@ -0,0 +1,80 @@
+//Introduction
+
+console.log("Hello JavaScript");
+console.log("My name is MD AFRID FOISAL OPU .");
+console.log("I am excited to learn JavaScript which is similer to CPP");
+
+
+// calculation 
+
+
+console.log("Addition : ", 50+69);
+console.log("Substraction : ", 50 - 34);
+console.log("Multiplication : ", 50 * 34);
+console.log("Division : ", (50 / 34).toFixed(2) );
+console.log("Modulus : ", 50 % 34);
+
+
+//student information 
+
+
+let studentName = "Afrid Foisal Opu";
+let age = 22;
+let country = " Bangladesh ";
+let isStudent = true;
+
+console.log("Student Name : ", studentName);
+console.log("Age : ", age);
+console.log("Country : ", country);
+console.log("Is Student : ", isStudent);
+
+let a = 100;
+let b = " Programming ";
+let c = true;
+let d = false;
+let e = 3.1416;
+
+
+console.log("A : ", typeof a);
+console.log("B : ", typeof b);
+console.log("C : ", typeof c);
+console.log("D : ", typeof d);
+console.log("E : ", typeof e);
+
+
+let n1 = 50;
+let n2 = 60;
+
+
+console.log("Number1 = ", n1);
+console.log("Number2 = ", n2);
+
+console.log();
+
+console.log("Addition : ", n1 + n2);
+console.log("Substraction : " , n1 - n2);
+console.log("Multiplication : " , n1 * n2);
+console.log("Division : " , (n1 / n2).toFixed(2));
+console.log("Modulus : " , n1 % n2);
+
+
+let score = 50;
+
+score += 10;
+console.log("After +=10 : " , score);
+
+
+score -= 5;
+console.log("After -=5 : " , score);
+
+
+score *= 10;
+console.log("After *=10 : " , score);
+
+
+score /= 10;
+console.log("After /=10 : " , score);
+
+
+score %= 4;
+console.log("After %=5 : " , score);
---
+++ summation.js
@@ -0,0 +1,15 @@
+const jerseyPrice = 400;
+const bootPrice = 1600;
+
+const totalPrice = jerseyPrice + bootPrice;
+const offer = totalPrice / 4;
+
+console.log("After_getting_offer",offer);
+
+
+console.log(totalPrice);
+
+// After "." to fix number we should use toFixed(3)
+// for converting number parseInt parseFloat
+
+
